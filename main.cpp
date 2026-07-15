e11717a27eff5e0829babaeb7b488b32b2e38f70
Sampanna Puri <ixsampanna@gmail.com>
2026-07-15 18:52:42 +0545

    trying to add bg

--- main.cpp
+++ main.cpp
@@ -6,26 +6,41 @@ int main()
     float radius=20;
     float vx=2,vy=2
     ;
-    Color color=WHITE;
+    Color colors[]={RED,GREEN,BLUE,YELLOW,PURPLE,ORANGE};
+    int colorindex=0;
+    Color color=colors[colorindex];
+    
     
     InitWindow(800,600,"Bouncing ball");
+    Texture2D bg=  LoadTexture("bg.jpg");
     SetTargetFPS(60);
     while(!WindowShouldClose())
     {
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            colorindex++;
+            color=colors[colorindex];
+            if(colorindex>=6)colorindex=0;
+
+        }
         
         x+=vx;
         y+=vy;
         if(x+radius>=800 || x-radius<=0) vx=-vx;
         if(y+radius>=600 || y-radius<=0) vy=-vy;
         BeginDrawing();
-
         ClearBackground(BLACK);
+        DrawTexture(bg,0,0,WHITE);
+
+        
         DrawCircle(x,y,radius,color);
+            DrawText("Click space to change color", 10, 10, 20, GRAY);
         
-        
+    
         EndDrawing();
 
     }
+    UnloadTexture(bg);
     
     CloseWindow();
 }
