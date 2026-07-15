3250b127d057afc340309f85cc8d2b105958750f
Sampanna Puri <ixsampanna@gmail.com>
2026-07-15 18:01:51 +0545

    bouncin ball completed

--- main.cpp
+++ main.cpp
@@ -2,15 +2,27 @@
 #include<raylib.h>
 int main()
 {
-    int x=400,y=300;
+    float x=400,y=300;
     float radius=20;
+    float vx=2,vy=2
+    ;
     Color color=WHITE;
+    
     InitWindow(800,600,"Bouncing ball");
+    SetTargetFPS(60);
     while(!WindowShouldClose())
     {
+        
+        x+=vx;
+        y+=vy;
+        if(x+radius>=800 || x-radius<=0) vx=-vx;
+        if(y+radius>=600 || y-radius<=0) vy=-vy;
         BeginDrawing();
+
         ClearBackground(BLACK);
         DrawCircle(x,y,radius,color);
+        
+        
         EndDrawing();
 
     }
